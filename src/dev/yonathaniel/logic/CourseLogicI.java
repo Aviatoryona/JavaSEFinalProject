7ca31c18918df9c98ed9bdcbeef83948124138fb
Aviator Yonathaniel <aviatoryona67@gmail.com>
2020-08-11 16:21:41 +0300

    Initial Commit

---
+++ src/dev/yonathaniel/logic/CourseLogicI.java
@@ -0,0 +1,20 @@
+package dev.yonathaniel.logic;
+
+import dev.yonathaniel.model.Course;
+import dev.yonathaniel.model.Teacher;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CourseLogicI {
+
+    boolean add(Course course) throws SQLException;
+
+    boolean update(Course course) throws SQLException;
+
+    boolean delete(Course course) throws SQLException;
+
+    Course find(int id) throws SQLException;
+
+    List<Course> findAll() throws SQLException;
+}
